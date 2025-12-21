a0acebcb44760353ad6e31892a609bf2d0c3943c
Yuvadi29 <adityatrivedi612@gmail.com>
2025-12-21 16:11:48 +0530

    fix: Adding OutputFileTracingExcludes for Serverless function file exceed limit

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,11 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   serverExternalPackages: ["chromadb"],
+
+  outputFileTracingExcludes: {
+    "*": [".next/export-detail.json"],
+    "/api/docs": ["./.next/cache/**/*"]
+  },
 };
 
 export default nextConfig;
